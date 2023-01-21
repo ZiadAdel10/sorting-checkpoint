b3eaecf87a84f14bcd7fff8a05d29adf67bbaf93
ZiadAdel10 <ziad.adel.mahmoud@gmail.com>
2023-01-21 20:18:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const calcResult = function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
+
+var insertion = [2, 4, 6, 7, 3, 8, 4, 5, 1];
